d8fb5740c074fe2bb42b7eb7a8a30a620361595a
Nedinator <imnedinator@gmail.com>
2026-03-26 00:05:12 -0500

    chore(models): updated guildschema to include a validator for how long the excluded channel id array can be; should test it

--- src/models/Guild.ts
+++ src/models/Guild.ts
@@ -5,7 +5,17 @@ const GuildSchema = new Schema<GuildDocType>({
   guildID: { type: String, required: true, index: true },
   notificationOn: { type: Boolean, default: false },
   notificationChannelID: { type: String, default: "" },
-  excludedChannelIDs: { type: [], default: [] },
+  excludedChannelIDs: {
+    type: [],
+    default: [],
+    //Added to ensure this doesn't get recklessly long. May be extended in the future
+    validate: {
+      validator: function (v: string[]) {
+        return v.length <= 100;
+      },
+      message: "Too many excluded channels (the limit is 100)",
+    },
+  },
 });
 
 const GuildDocument = model<GuildDocType>("GuildDocument", GuildSchema);
